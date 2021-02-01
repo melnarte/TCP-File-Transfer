c4f53d026e818a2acee19cdcecbd06667bed7122
Artem MELNYK <melnyk.artem@qenvi.com>
2021-02-01 14:44:14 +0100

    mutuall adding fonctionnality

--- file_client.cpp
+++ file_client.cpp
@@ -9,68 +9,91 @@
 
 using namespace std;
 
-class Client_socket{
+class Client_socket
+{
     fstream file;
 
     int PORT;
-    
-    int general_socket_descriptor;
+
+    int current_socket_descriptor;
 
     struct sockaddr_in address;
     int address_length;
 
-    public:
-        Client_socket(){
-            create_socket();
-            PORT = 8050;
+public:
+    Client_socket()
+    {
+        create_socket();
+        PORT = 8050;
 
-            address.sin_family = AF_INET;
-            address.sin_port = htons( PORT );
-            address_length = sizeof(address);
-            if(inet_pton(AF_INET, "127.0.0.1", &address.sin_addr)<=0) { 
-                cout<<"[ERROR] : Invalid address\n";
-            }
-
-            create_connection();
-            
-            file.open(".//Data//Client//client_text.txt", ios::out | ios::trunc | ios::binary);
-            if(file.is_open()){
-                cout<<"[LOG] : File Creted.\n";
-            }
-            else{
-                cout<<"[ERROR] : File creation failed, Exititng.\n";
-                exit(EXIT_FAILURE);
-            }
+        address.sin_family = AF_INET;
+        address.sin_port = htons( PORT );
+        address_length = sizeof(address);
+        if(inet_pton(AF_INET, "127.0.0.1", &address.sin_addr)<=0)
+        {
+            cout<<"[ERROR] : Invalid address\n";
         }
 
-        void create_socket(){
-            if ((general_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
-                perror("[ERROR] : Socket failed.\n");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Socket Created Successfully.\n";
-        }
+        create_connection();
 
-        void create_connection(){
-            if (connect(general_socket_descriptor, (struct sockaddr *)&address, sizeof(address)) < 0) { 
-                perror("[ERROR] : connection attempt failed.\n");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Connection Successfull.\n";
+        file.open(".//Data//Client//client_text.txt", ios::out | ios::trunc | ios::binary);
+        if(file.is_open())
+        {
+            cout<<"[LOG] : File Creted.\n";
         }
+        else
+        {
+            cout<<"[ERROR] : File creation failed, Exititng.\n";
+            exit(EXIT_FAILURE);
+        }
+    }
 
-        void receive_file(){
-            char buffer[1024] = {};
-            int valread = read(general_socket_descriptor , buffer, 1024);
-            cout<<"[LOG] : Data received "<<valread<<" bytes\n";
-            cout<<"[LOG] : Saving data to file.\n";
-            
-            file<<buffer;
-            cout<<"[LOG] : File Saved.\n";
+    void create_socket()
+    {
+        if ((current_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+            perror("[ERROR] : Socket failed.\n");
+            exit(EXIT_FAILURE);
         }
+        cout<<"[LOG] : Socket Created Successfully.\n";
+    }
+
+    void create_connection()
+    {
+        if (connect(current_socket_descriptor, (struct sockaddr *)&address, sizeof(address)) < 0)
+        {
+            perror("[ERROR] : connection attempt failed.\n");
+            exit(EXIT_FAILURE);
+        }
+        cout<<"[LOG] : Connection Successfull.\n";
+    }
+
+    void receive_file()
+    {
+        char buffer[1024] = {};
+        int valread = read(current_socket_descriptor, buffer, 1024);
+        cout<<"[LOG] : Data received "<<valread<<" bytes\n";
+        cout<<"[LOG] : Saving data to file.\n";
+
+        file<<buffer;
+        cout<<"[LOG] : File Saved.\n";
+    }
+    void transmit_file()
+    {
+        std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        cout<<"[LOG] : Transmission Data Size "<<contents.length()<<" Bytes.\n";
+
+        cout<<"[LOG] : Sending...\n";
+
+        int bytes_sent = send(current_socket_descriptor, contents.c_str(), contents.length(), 0 );
+        cout<<"[LOG] : Transmitted Data Size "<<bytes_sent<<" Bytes.\n";
+
+        cout<<"[LOG] : File Transfer Complete.\n";
+    }
 };
 
-int main(){
+int main()
+{
     Client_socket C;
     C.receive_file();
     return 0;
--- file_server.cpp
+++ file_server.cpp
@@ -9,87 +9,111 @@
 
 using namespace std;
 
-class Server_socket{
+class Server_socket
+{
     fstream file;
 
     int PORT;
-    
+
     int general_socket_descriptor;
-    int new_socket_descriptor;
+    int current_socket_descriptor;
 
     struct sockaddr_in address;
     int address_length;
 
-    public:
-        Server_socket(){
-            create_socket();
-            PORT = 8050;
+public:
+    Server_socket()
+    {
+        create_socket();
+        PORT = 8050;
 
-            address.sin_family = AF_INET; 
-            address.sin_addr.s_addr = INADDR_ANY; 
-            address.sin_port = htons( PORT );
-            address_length = sizeof(address);
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons( PORT );
+        address_length = sizeof(address);
 
-            bind_socket();
-            set_listen_set();
-            accept_connection();
+        bind_socket();
+        set_listen_set();
+        accept_connection();
 
-            file.open(".//Data//Server//server_text.txt", ios::in | ios::binary);
-            if(file.is_open()){
-                cout<<"[LOG] : File is ready to Transmit.\n";
-            }
-            else{
-                cout<<"[ERROR] : File loading failed, Exititng.\n";
-                exit(EXIT_FAILURE);
-            }
+        file.open(".//Data//Server//server_text.txt", ios::in | ios::binary);
+        if(file.is_open())
+        {
+            cout<<"[LOG] : File is ready to Transmit.\n";
         }
-
-        void create_socket(){
-            if ((general_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
-                perror("[ERROR] : Socket failed");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Socket Created Successfully.\n";
+        else
+        {
+            cout<<"[ERROR] : File loading failed, Exititng.\n";
+            exit(EXIT_FAILURE);
         }
+    }
 
-        void bind_socket(){
-            if (bind(general_socket_descriptor, (struct sockaddr *)&address, sizeof(address))<0) {
-                perror("[ERROR] : Bind failed");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Bind Successful.\n";
+    void create_socket()
+    {
+        if ((general_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("[ERROR] : Socket failed");
+            exit(EXIT_FAILURE);
         }
+        cout<<"[LOG] : Socket Created Successfully.\n";
+    }
 
-        void set_listen_set(){
-            if (listen(general_socket_descriptor, 3) < 0) {
-                perror("[ERROR] : Listen");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Socket in Listen State (Max Connection Queue: 3)\n";
+    void bind_socket()
+    {
+        if (bind(general_socket_descriptor, (struct sockaddr *)&address, sizeof(address))<0)
+        {
+            perror("[ERROR] : Bind failed");
+            exit(EXIT_FAILURE);
         }
+        cout<<"[LOG] : Bind Successful.\n";
+    }
 
-        void accept_connection(){
-            if ((new_socket_descriptor = accept(general_socket_descriptor, (struct sockaddr *)&address, (socklen_t*)&address_length))<0) { 
-                perror("[ERROR] : Accept");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Connected to Client.\n";
+    void set_listen_set()
+    {
+        if (listen(general_socket_descriptor, 3) < 0)
+        {
+            perror("[ERROR] : Listen");
+            exit(EXIT_FAILURE);
         }
+        cout<<"[LOG] : Socket in Listen State (Max Connection Queue: 3)\n";
+    }
 
-        void transmit_file(){
-            std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-            cout<<"[LOG] : Transmission Data Size "<<contents.length()<<" Bytes.\n";
-
-            cout<<"[LOG] : Sending...\n";
-
-            int bytes_sent = send(new_socket_descriptor , contents.c_str() , contents.length() , 0 );
-            cout<<"[LOG] : Transmitted Data Size "<<bytes_sent<<" Bytes.\n";
-
-            cout<<"[LOG] : File Transfer Complete.\n";
+    void accept_connection()
+    {
+        if ((current_socket_descriptor = accept(general_socket_descriptor, (struct sockaddr *)&address, (socklen_t*)&address_length))<0)
+        {
+            perror("[ERROR] : Accept");
+            exit(EXIT_FAILURE);
         }
+        cout<<"[LOG] : Connected to Client.\n";
+    }
+
+    void receive_file()
+    {
+        char buffer[1024] = {};
+        int valread = read(current_socket_descriptor, buffer, 1024);
+        cout<<"[LOG] : Data received "<<valread<<" bytes\n";
+        cout<<"[LOG] : Saving data to file.\n";
+
+        file<<buffer;
+        cout<<"[LOG] : File Saved.\n";
+    }
+    void transmit_file()
+    {
+        std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        cout<<"[LOG] : Transmission Data Size "<<contents.length()<<" Bytes.\n";
+
+        cout<<"[LOG] : Sending...\n";
+
+        int bytes_sent = send(current_socket_descriptor, contents.c_str(), contents.length(), 0 );
+        cout<<"[LOG] : Transmitted Data Size "<<bytes_sent<<" Bytes.\n";
+
+        cout<<"[LOG] : File Transfer Complete.\n";
+    }
 };
 
-int main(){
+int main()
+{
     Server_socket S;
     S.transmit_file();
     return 0;
